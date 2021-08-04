0bc3fa7164e5386fb3f952f566dd9b7242eef944
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-04 10:44:52 -0600

    Build test and model class method for average age of players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :team
+
+  def self.average_age
+    average(:age).to_i
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe Player do
   describe 'relationships' do
     it { should belong_to(:team) }
   end
+
+  before(:each) do
+    @team_1 = Team.create!(hometown: 'La Grande', nickname: 'fun times')
+    @team_2 = Team.create!(hometown: 'Detroit', nickname: 'winners')
+
+    @player_1 = @team_1.players.create!(name: 'Dame', age: 30)
+    @player_2 = @team_1.players.create!(name: 'CJ', age: 30)
+    @player_3 = @team_1.players.create!(name: 'Nurk', age: 28)
+
+    @player_4 = @team_2.players.create!(name: 'LeBron', age: 35)
+    @player_5 = @team_2.players.create!(name: 'MJ', age: 45)
+    @player_6 = @team_2.players.create!(name: 'Shaq', age: 40)
+  end
+
+  describe 'class_methods' do
+    describe '#average_age' do
+      it 'returns the average age of all players' do
+        expected = (@player_1.age + @player_2.age + @player_3.age + @player_4.age + @player_5.age + @player_6.age) / (@team_1.players.count + @team_2.players.count)
+
+        expect(Player.average_age).to eq(expected)
+      end
+    end
+  end
 end
